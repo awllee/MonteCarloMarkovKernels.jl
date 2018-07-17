a5fd584259739de742e33645fbfba724a94a996d
Anthony Lee <awllee@gmail.com>
2018-07-17 13:14:33 +0100

    travis, appveyor, coverage

---
+++ README.md
@@ -0,0 +1,23 @@
+# Monte Carlo Markov Kernels
+
+[![Build Status](https://travis-ci.org/awllee/MonteCarloMarkovKernels.jl.svg?branch=master)](https://travis-ci.org/awllee/NonUniformRandomVariateGeneration.jl)
+[![Build status](https://ci.appveyor.com/api/projects/status/ve73suox2nrlqvu9?svg=true)](https://ci.appveyor.com/project/awllee/montecarlomarkovkernels-jl)
+[![Coverage Status](https://coveralls.io/repos/github/awllee/MonteCarloMarkovKernels.jl/badge.svg?branch=master)](https://coveralls.io/github/awllee/MonteCarloMarkovKernels.jl?branch=master)
+[![codecov.io](http://codecov.io/github/awllee/MonteCarloMarkovKernels.jl/coverage.svg?branch=master)](http://codecov.io/github/awllee/MonteCarloMarkovKernels.jl?branch=master)
+
+This package provides some simple Monte Carlo Markov kernel implementations.
+
+Currently implemented:
+
+* Random walk Metropolis
+* Adaptive Metropolis
+
+To be implemented in the future:
+
+* Metropolis-adjusted Langevin
+* Hamiltonian Monte Carlo
+
+The package also provides implementations of some consistent asymptotic variance estimators:
+
+* Batch means
+* Spectral variance
--- test/visualize_test.jl
+++ test/visualize_test.jl
@@ -3,10 +3,6 @@ using StaticArrays
 using StatsBase
 if VERSION.minor == 7 import Statistics.mean end
 using Compat.Test
-# using Plots
-# Plots.gr()
-
-# savefigures = false
 
 zero2 = [0.0, 0.0]
 μ1 = [-1.0, 0.0]
@@ -37,8 +33,6 @@ simulateChain!(chain, P_AM, Szero2)
 @test mean(chain) ≈ μ1 atol = 0.01
 @test MonteCarloMarkovKernels.cov(chain) ≈ Σ1 atol = 0.05
 
-!isinteractive() && (ENV["GKSwstype"] = "100")
-
 vs = Vector{Vector{Float64}}(2)
 for i = 1:2
   vs[i] = (x->x[i]).(chain)
@@ -48,23 +42,12 @@ d1(x) = 1/sqrt(2*π*Σ1[1,1])*exp(-1/2/Σ1[1,1]*(x-μ1[1])^2)
 d2(x) = 1/sqrt(2*π*Σ1[2,2])*exp(-1/2/Σ1[2,2]*(x-μ1[2])^2)
 
 xs, ys = kde(vs[1], P_AM(:acceptanceRate))
-# plot(xs, ys)
-# plot!(xs, d1.(xs), color="red")
-# savefigures && savefig("kde1.png")
 @test ys ≈ d1.(xs) atol=0.05
 
 xs, ys = kde(vs[2], P_AM(:acceptanceRate))
-# plot(xs, ys)
-# plot!(xs, d2.(xs), color="red")
-# savefigures && savefig("kde2.png")
 @test ys ≈ d2.(xs) atol=0.05
 
 xs, ys, f1 = kde(vs[1], vs[2], P_AM(:acceptanceRate))
-# contour(xs, ys, f1)
-# contour!(xs, ys, (x,y)->exp(logtarget((SVector{2,Float64}(x,y)))))
-# savefigures && savefig("kde12.png")
-
-# plot(autocor(vs[1]))
-# savefigures && savefig("acf1.png")
-# plot(autocor(vs[2]))
-# savefigures && savefig("acf2.png")
+M1 = broadcast(f1, xs, ys')
+M2 = broadcast((x,y)->exp(logtarget((SVector{2,Float64}(x,y)))), xs, ys')
+@test M1 ≈ M2 atol=0.05
